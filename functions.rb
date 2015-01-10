4706a76c14906ed23dcff9ea4e4925bac4e45502
Garry Hicks <garryhicks21@gmail.com>
2015-01-10 00:38:05 +0000

    added logic and methods for evaluation

--- functions.rb
+++ functions.rb
@@ -4,7 +4,18 @@ require 'byebug'
 def generate_question
   x = (1..20).to_a.sample
   y = (1..20).to_a.sample
-  puts "What is #{x} plus #{y}"
+   puts "What is #{x} plus #{y}?"
+  @correct_answer = x + y
 end
 
-# p generate_question
+def prompt_player(player)
+  puts "#{player} - you're up!"
+end
+
+def verify_answer(answer)
+  answer == @correct_answer
+end
+
+def switch_player
+  @current_player == @player1 ? @current_player = @player2 : @current_player = @player1
+end
--- game.rb
+++ game.rb
@@ -1,25 +1,43 @@
 require 'byebug'
 require './functions.rb'
 
-@player_one_lives == 3
-@player_two_lives == 3
-
+@player_one_lives = 3
+@player_two_lives = 3
+@current_player = @player1
 def intro
   puts "Please enter your name, Player 1: "
-  player1 = gets.chomp
-  puts "Thank you, #{player1}"
+  @player1 = gets.chomp
+  puts "Thank you, #{@player1}"
   puts "Please enter your name, Player 2: "
-  player2 = gets.chomp
-  puts "Thank you, #{player1}"
+  @player2 = gets.chomp
+  puts "Thank you, #{@player2}"
   puts "Welcome to the game!"
 end
 
 def main
-  p intro
-  p 
-  puts generate_question
-  prompt_player
-  verify_answer
+  intro
+  # p 
+  while @player_one_lives != 0 && @player_two_lives != 0
+    generate_question
+    prompt_player(@player1)
+    answer = gets.chomp.to_i
+    result = verify_answer(answer)
+    switch_player
+    if result == false 
+      if @current_player == @player1
+        @player_one_lives -= 1
+      else
+        @player_two_lives -= 1
+      end
+      puts "#{@player1}: #{@player_one_lives} lives"
+      puts "#{@player2}: #{@player_two_lives} lives"
+      puts "bohoo wrong!"
+    else 
+      puts "good job!"
+    end
+
+  end 
+  puts "Game over!"
 end
 
-p run()
+main
